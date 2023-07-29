e14e00d615c60f3c2cb22d95a96c39bebd4ea852
Hui Feng <huifeng001@gmail.com>
2023-07-29 20:56:28 +0100

    updated set timeout and questions functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,21 +1,53 @@
 //connect to HTML
-let startBtn = document.querySelector("#start");
-let questionScreen = document.querySelector("#questions");
+let startBtn = document.querySelector("#start"); //start button
+let questionScreen = document.querySelector("#questions"); //question
+let questionTitle = document.querySelector("#question-title");
+let questionChoices = document.querySelector("#choices"); //choice div
+
+let startScreen = document.querySelector("#start-screen");
 
 // set up the visible and hide class in each step: start-> questions -> score
-startBtn.addEventListener("click", function (event) {
-  let questionScreenState = questionScreen.getAttribute("class");
+startBtn.addEventListener("click", showQuestions);
 
+function showQuestions() {
+  let questionScreenState = questionScreen.getAttribute("class");
   if (questionScreenState === "hide")
     questionScreenState = questionScreen.setAttribute("class", "visible");
-});
+  console.log("removed");
+  startScreen.remove();
+  setTimeout();
+}
 
-//function question
-//if questions is visible
-//create a container containing the h2 and 4 choices
-// set the h2 to the'question', and the 4 choices to the answers in random order
-//inject the code block into the HTML using appendChild
+//draw random function questions
+function jsQuestionDraw(event) {
+  // set the h2 to the'question', and the 4 choices to the answers in random
+  let randomQuestionArray = shuffle(quizQuestions);
+  let count = 0;
+  //title
+  questionTitle.textContent = randomQuestionArray[count].question;
 
+  //choices
+  // Step 1: Create a new <div> element
+  const newDiv1 = document.createElement("div");
+  const newDiv2 = document.createElement("div");
+  const newDiv3 = document.createElement("div");
+  const newDiv4 = document.createElement("div");
+
+  // Step 2: set attributes or content for the new <div>
+  let choice1 = quizQuestions.wrongAnswer1;
+  let choice2 = quizQuestions.wrongAnswer2;
+  let choice3 = quizQuestions.wrongAnswer3;
+  let choice4 = quizQuestions.rightAnswer;
+  let choiceArray = [choice1, choice2, choice3, choice4];
+  let randomChoiceArray = shuffle(choiceArray);
+
+  for (let j = 0; j < 4; j++) {
+    newDiv[j].textContent = randomChoiceArray[j];
+
+    // Step 3: Append the new <div> as a child of the target parent element
+    questionChoices.appendChild(newDiv);
+  }
+}
 //wrong answer clicked, show 'wrong' and jump to the next question
 
 //right answer clicked, show 'correct!' and jump to the next question
@@ -28,3 +60,29 @@ startBtn.addEventListener("click", function (event) {
 //create a container containing the h2(score) and 2 buttons: Go back, Clear High Score
 //button 'go back': back to main page
 //button 'clear high score'
+
+//timer function: display score screen after 20 seconds (20000 milliseconds) delay
+setTimeout(function () {
+  let time = document.querySelector("#time");
+  const countdownSeconds = 20;
+  countdownTimer(countdownSeconds);
+  time.textContent = "countDown";
+  console.log("This message will be printed after 2 seconds.");
+}, 20000);
+
+//countdown timer
+function countdownTimer(seconds) {
+  function updateCountdown() {
+    console.log(seconds);
+    seconds--;
+
+    if (seconds >= 0) {
+      setTimeout(updateCountdown, 1000); // Call the function again after 1 second (1000 milliseconds)
+    } else {
+      console.log("Countdown complete!");
+    }
+  }
+
+  updateCountdown(); // Start the countdown
+}
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,68 @@
 // create 5 questions as objects, and put them into an array
 //the object should be in the following format
-// {question:
-// wrongAnswer1:
-// wrongAnswer2:
-// wrongAnswer3:
-// rightAnswer:
+// {question:"",
+// wrongAnswer1:"",
+// wrongAnswer2:"",
+// wrongAnswer3:"",
+// rightAnswer:"",
 // }
+let quizQuestions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    wrongAnswer1: "<js>",
+    wrongAnswer2: "<scripting>",
+    wrongAnswer3: "<javascript>",
+    rightAnswer: "<script>",
+  },
+
+  {
+    question: "What does DOM stand for?",
+    wrongAnswer1: "Diverging Occlusion Method",
+    wrongAnswer2: "Differentiated Operational Module",
+    wrongAnswer3: "Days On Market",
+    rightAnswer: "Document Object Model",
+  },
+  {
+    question: "Which of the following is a type of JavaScript pop up box?",
+    wrongAnswer1: "Submit",
+    wrongAnswer2: "Cancel",
+    wrongAnswer3: "Reject",
+    rightAnswer: "Alert",
+  },
+  {
+    question: "What statement ends JavaScript function execution?",
+    wrongAnswer1: "stop",
+    wrongAnswer2: "finalize",
+    wrongAnswer3: "end",
+    rightAnswer: "return",
+  },
+  {
+    question:
+      "Which operator determines if values are equal in value and type in JavaScript?",
+    wrongAnswer1: "=+",
+    wrongAnswer2: "==",
+    wrongAnswer3: "=",
+    rightAnswer: "===",
+  },
+];
+
+//shuffle array in random order
+function shuffle(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]; //what does this mean?
+  }
+
+  return array;
+}
